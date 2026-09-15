55f14db8ee83a824eb8d67a3005a458e1b00a1c2
一田昂灼 <kokouya2711@gmail.com>
2026-09-15 12:15:14 +0000

    トーストの自動消去時間を1秒に短縮

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 const TOAST_LIMIT = 20;
 const TOAST_REMOVE_DELAY = 1000;
-const TOAST_AUTO_DISMISS = 4000;
+const TOAST_AUTO_DISMISS = 1000;
 
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
